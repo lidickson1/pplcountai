a4d5d3cd3587828a1d1bda0a575197cce359b68b
lidickson1 <52766315+lidickson1@users.noreply.github.com>
2020-05-02 23:54:28 -0400

    Backend restructure
    Now the backend can be used as a module

--- backend.js
+++ backend.js
@@ -3,171 +3,36 @@ const url =
     "mongodb+srv://dickson:123@pplcountai-p9qbg.mongodb.net/test?retryWrites=true&w=majority";
 let db;
 
-//returns a promise. If connected then nothing is returned, or else an error is returned
-function connect() {
-    return new Promise((resolve, reject) => {
-        MongoClient.connect(url, { useUnifiedTopology: true })
-            .then((cluster) => {
-                db = cluster.db("database");
-                resolve();
-            })
-            .catch((err) => reject(err));
-    });
-}
+module.exports = {
+    //returns a promise. If connected then nothing is returned, or else an error is returned
+    connect: function () {
+        return new Promise((resolve, reject) => {
+            MongoClient.connect(url, { useUnifiedTopology: true })
+                .then((cluster) => {
+                    db = cluster.db("database");
+                    resolve();
+                })
+                .catch((err) => reject(err));
+        });
+    },
 
-function login(email, pass) {
-    return new Promise((resolve, reject) => {
-        db.collection("business_accounts").findOne(
-            { emailAddress: email, pass: pass },
-            function (err, result) {
-                if (err || result === null) {
-                    throw new Error("Login Failed");
-                } else {
-                    resolve(result);
-                }
-            }
-        );
-    });
-}
-
-function createBusinessAccount(
-    email,
-    pass,
-    companyName,
-    address,
-    postalCode,
-    maxNumberOfPeople,
-    description
-) {
-    let businessObject = {
-        emailAddress: email,
-        pass: pass,
-        companyName: companyName,
-        address: address,
-        postalCode: postalCode,
-        maxNumberOfPeople: maxNumberOfPeople,
-        description: description,
-    };
-    db.collection("business_accounts")
-        .find({ emailAddress: email, pass: pass })
-        .toArray(function (err, result) {
-            if (err) {
-                throw err;
-            }
-
-            if (result.length > 0) {
-                throw new Error("Business already exists!");
-            }
-            db.collection("business_accounts").insertOne(
-                businessObject,
-                function (err, res) {
-                    if (err) {
-                        throw err;
+    login: function (email, pass) {
+        return new Promise((resolve, reject) => {
+            db.collection("business_accounts").findOne(
+                { emailAddress: email, pass: pass },
+                function (err, result) {
+                    if (err || result === null) {
+                        reject("Login Failed");
+                    } else {
+                        console.log("success");
+                        resolve(result);
                     }
                 }
             );
         });
-    return true;
-}
+    },
 
-function businessExists(email, pass) {
-    return new Promise(function (resolve, reject) {
-        db.collection("business_accounts")
-            .find({ emailAddress: email, pass: pass })
-            .toArray(function (err, result) {
-                if (err) {
-                    resolve(false);
-                }
-                if (result.length == 0) {
-                    reject("Business doesn't exist!");
-                }
-                resolve(result);
-            });
-    });
-}
-
-function deleteBusinessAccount(email, pass) {
-    businessExists(email, pass).then((result) => {
-        db.collection("business_accounts").deleteOne(
-            { emailAddress: email, pass: pass },
-            function (err, obj) {
-                if (err) {
-                    throw err;
-                }
-            }
-        );
-    });
-}
-
-//allows company to change number of people in business
-function updateNumberOfPeople(email, pass, numberOfPeople) {
-    businessExists(email, pass).then((result) => {
-        let newValues = { $set: { numberOfPeople: numberOfPeople } };
-        db.collection("business_accounts").updateOne(
-            { emailAddress: email, pass: pass },
-            newValues,
-            function (err, res) {
-                if (err) {
-                    throw err;
-                }
-            }
-        );
-    });
-}
-
-//allows company to increase number of people in business by one
-function incrementNumberOfPeople(email, pass) {
-    businessExists(email, pass).then((result) => {
-        let num = result[0].numberOfPeople;
-
-        let newValues = { $set: { numberOfPeople: num + 1 } };
-        db.collection("business_accounts").updateOne(
-            { emailAddress: email, pass: pass },
-            newValues,
-            function (err, res) {
-                if (err) {
-                    throw err;
-                }
-            }
-        );
-    });
-}
-
-//allows company to decrease number of people in business by one
-function decrementNumberOfPeople(email, pass) {
-    businessExists(email, pass).then((result) => {
-        let num = result[0].numberOfPeople;
-
-        let newValues = { $set: { numberOfPeople: num - 1 } };
-        db.collection("business_accounts").updateOne(
-            { emailAddress: email, pass: pass },
-            newValues,
-            function (err, res) {
-                if (err) {
-                    throw err;
-                }
-            }
-        );
-    });
-}
-
-//allows company to change number of people in business
-function updateDescription(email, pass, description) {
-    businessExists(email, pass).then((result) => {
-        let newValues = { $set: { description: description } };
-        db.collection("business_accounts").updateOne(
-            { emailAddress: email, pass: pass },
-            newValues,
-            function (err, res) {
-                if (err) {
-                    throw err;
-                }
-            }
-        );
-    });
-}
-
-function returnArrayOfQueries(stringQuery) {
+    companyList: function (stringQuery) {
         let query = new RegExp(stringQuery, "i");
         return new Promise((resolve, reject) => {
             db.collection("business_accounts")
@@ -188,13 +53,150 @@ function returnArrayOfQueries(stringQuery) {
                     }
                 });
         });
-    }
+    },
 
-// connect()
-//     .then(() => {
-//         login("hah@gmail.com", "r3q23rq");
-//         console.log("Login success!");
-//     })
+    createBusinessAccount: function (
+        email,
+        pass,
+        companyName,
+        address,
+        postalCode,
+        maxNumberOfPeople,
+        description
+    ) {
+        let businessObject = {
+            emailAddress: email,
+            pass: pass,
+            companyName: companyName,
+            address: address,
+            postalCode: postalCode,
+            maxNumberOfPeople: maxNumberOfPeople,
+            description: description,
+        };
+        db.collection("business_accounts")
+            .find({ emailAddress: email, pass: pass })
+            .toArray(function (err, result) {
+                if (err) {
+                    reject(err);
+                }
+
+                if (result.length > 0) {
+                    reject("Business already exists!");
+                }
+                db.collection("business_accounts").insertOne(
+                    businessObject,
+                    function (err, res) {
+                        if (err) {
+                            reject(err);
+                        }
+                    }
+                );
+            });
+        return true;
+    },
+
+    businessExists: function (email, pass) {
+        return new Promise(function (resolve, reject) {
+            db.collection("business_accounts")
+                .find({ emailAddress: email, pass: pass })
+                .toArray(function (err, result) {
+                    if (err) {
+                        resolve(false);
+                    }
+                    if (result.length == 0) {
+                        reject("Business doesn't exist!");
+                    }
+                    resolve(result);
+                });
+        });
+    },
+
+    deleteBusinessAccount: function (email, pass) {
+        businessExists(email, pass).then((result) => {
+            db.collection("business_accounts").deleteOne(
+                { emailAddress: email, pass: pass },
+                function (err, obj) {
+                    if (err) {
+                        reject(err);
+                    }
+                }
+            );
+        });
+    },
+
+    //allows company to change number of people in business
+    updateNumberOfPeople: function (email, pass, numberOfPeople) {
+        businessExists(email, pass).then((result) => {
+            let newValues = { $set: { numberOfPeople: numberOfPeople } };
+            db.collection("business_accounts").updateOne(
+                { emailAddress: email, pass: pass },
+                newValues,
+                function (err, res) {
+                    if (err) {
+                        reject(err);
+                    }
+                }
+            );
+        });
+    },
+
+    //allows company to increase number of people in business by one
+    incrementNumberOfPeople: function (email, pass) {
+        businessExists(email, pass).then((result) => {
+            let num = result[0].numberOfPeople;
+
+            let newValues = { $set: { numberOfPeople: num + 1 } };
+            db.collection("business_accounts").updateOne(
+                { emailAddress: email, pass: pass },
+                newValues,
+                function (err, res) {
+                    if (err) {
+                        reject(err);
+                    }
+                }
+            );
+        });
+    },
+
+    //allows company to decrease number of people in business by one
+    decrementNumberOfPeople: function (email, pass) {
+        businessExists(email, pass).then((result) => {
+            let num = result[0].numberOfPeople;
+
+            let newValues = { $set: { numberOfPeople: num - 1 } };
+            db.collection("business_accounts").updateOne(
+                { emailAddress: email, pass: pass },
+                newValues,
+                function (err, res) {
+                    if (err) {
+                        reject(err);
+                    }
+                }
+            );
+        });
+    },
+
+    //allows company to change number of people in business
+    updateDescription: function (email, pass, description) {
+        businessExists(email, pass).then((result) => {
+            let newValues = { $set: { description: description } };
+            db.collection("business_accounts").updateOne(
+                { emailAddress: email, pass: pass },
+                newValues,
+                function (err, res) {
+                    if (err) {
+                        reject(err);
+                    }
+                }
+            );
+        });
+    },
+};
+
+// module.exports
+//     .connect()
+//     .then(() => module.exports.companyList("Google"))
+//     .then((result) => console.log(result))
 //     .catch((err) => {
 //         throw err;
 //     });
