bbe439ba5d917eb9b92049c47c09fa018a6909b1
marwaahmed198 <58569044+marwaahmed198@users.noreply.github.com>
2020-05-03 01:26:57 -0400

    add google vision API file

---
+++ visionapi.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const app = express();
+
+const vision = require('@google-cloud/vision');
+
+
+const client = new vision.ImageAnnotatorClient();
+
+const request = {
+    
+        "image": {
+          "source": {
+            "imageUri": "gs://poeplecountaiimages/image3.jpg"
+          }
+        }
+      }
+
+
+function countFaces(){
+client.faceDetection(request)
+.then(results=>{
+    const faces = results[0].faceAnnotations;
+    const numFaces = faces.length;
+    console.log(`Found ${numFaces} face${numFaces === 1 ? '' : 's'}.`);
+    return faces;
+})
+.catch(err=>{
+    console.error('ERROR', err);
+});
+};
+
+
+countFaces();
+
+
+app.listen(5000, ()=>{
+    console.log('Express running on port 5000');
+});
+
+
+
+// app.get('/', (req, res)=>{
+//     res.send(`Found ${numFaces}`)
+// });
