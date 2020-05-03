262d184abe09124627c0bf032665e28f39010659
Kaiyi <kaiyiliu21@gmail.com>
2020-05-03 02:13:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -32,4 +32,85 @@ server.post("/list", function (request, response) {
         });
 });
 
+server.post("/create-business-account", function (request, response) {
+  backend
+        .createBusinessAccount(request.body.email, request.body.pass, 
+        request.body.companyName, request.body.address, request.body.postalCode,
+        request.body.maxNumberOfPeople, requestion.body.description)
+        .then((res) => {
+            response.status(200).json(res);
+        })
+        .catch((err) => {
+            response.status(401).send("Account failed to be created!");
+        });
+});
+
+server.post("/business-exists", function (request, response) {
+  backend
+        .businessExists(request.body.email, request.body.pass)
+        .then((result) => {
+            response.status(200).json(result);
+        })
+        .catch((err) => {
+            response.status(401).send("Error occurred!");
+        });
+});
+
+server.post("/delete-business-account", function (request, response) { 
+  backend
+        .deleteBusinessAccount(request.body.email, request.body.pass)
+        .then((obj) => {
+            response.status(200).json(obj)
+        })
+        .catch((err) => {
+            response.status(401).send("Account failed to be deleted!");
+        });
+});
+
+server.post("/update-number-of-people", function (request, response) {
+  backend
+        .updateNumberOfPeople(request.body.email, request.body.pass, 
+        request.body.numberOfPeople)
+        .then((res) => {
+            response.status(200).json(res);
+        })
+        .catch((err) => {
+            response.status(401).send("Failed to update number of people!");
+        });
+});
+
+server.post("/increment-number-of-people", function (request, response) {
+  backend
+        .incrementNumberOfPeople(request.body.email, request.body.pass)
+        .then((res) => {
+            response.status(200).json(res);
+        })
+        .catch((err) => {
+            response.status(401).send("Failed to increase number of people!");
+        });
+});
+
+server.post("/decrement-number-of-people", function (request, response) {
+  backend
+        .decrementNumberOfPeople(request.body.email, request.body.pass)
+        .then((res) => {
+            response.status(200).json(res);
+        })
+        .catch((err) => {
+            response.status(401).send("Failed to decrease number of people!");
+        });
+});
+
+server.post("/update-description", function (request, response) {
+  backend
+        .updateDescription(request.body.email, request.body.pass, 
+        request.body.description)
+        .then((res) => {
+            response.status(200).json(res);
+        })
+        .catch((err) => {
+            response.status(401).send("Failed to update desctiption!");
+        });
+});
+
 server.listen(server.get("port"));
