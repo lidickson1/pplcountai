35f08a857078e15a46a3dbb0268201475991626f
lidickson1 <52766315+lidickson1@users.noreply.github.com>
2020-05-03 00:53:21 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-//server.js
 const express = require("express");
 const server = express();
+const bodyParser = require("body-parser");
+const backend = require("./backend");
+
+server.use(bodyParser.json());
 
 //setting the port.
 server.set("port", process.env.PORT || 3000);
 
-// //Adding routes
-// server.get("/", (request, response) => {
-//     response.sendFile(__dirname + "/index.html");
-// });
-
-// server.get("/about-us", (request, response) => {
-//     response.sendFile(__dirname + "/about-us.html");
-// });
-
 server.use(express.static(__dirname));
 
-// server.get("/users", (request, response) => {
-//     response.json(users);
-// });
+server.post("/connect", function (request, response) {
+    backend
+        .connect()
+        .then(() => {
+            response.status(200).send("Connection successful!");
+        })
+        .catch((err) => {
+            response.status(401).send("Connection failed!");
+        });
+});
 
-//Binding to localhost://3000
+server.post("/list", function (request, response) {
+    backend
+        .companyList(request.body.query)
+        .then((result) => {
+            response.status(200).json(result);
+        })
+        .catch((err) => {
+            response.status(401).send("List failed!");
+        });
+});
+
 server.listen(server.get("port"));
